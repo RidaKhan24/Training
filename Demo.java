98ba9e11f46e8266fbb1147134018a6254d3b41c
RidaKhan24 <119613464+RidaKhan24@users.noreply.github.com>
2022-12-02 21:31:17 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Rida");
+		System.out.println("H.No 1-2-154/5/2\nRahmath nagar kazipet 506003");
+		
+
+	}
+
+}
